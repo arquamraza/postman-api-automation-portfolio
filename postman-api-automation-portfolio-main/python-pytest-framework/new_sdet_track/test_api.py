ddd2037d4295365ab1006823b540a824b8f279bb
arquamraza <arquamraza@github.com>
2026-09-11 21:41:21 -0700

    Remove duplicate folder

--- postman-api-automation-portfolio-main/python-pytest-framework/new_sdet_track/test_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def test_user_api():
-    url = "https://jsonplaceholder.typicode.com/users/1"
-    response = requests.get(url)
-    
-    assert response.status_code == 200
-    assert response.json()['name'] == "Leanne Graham"
